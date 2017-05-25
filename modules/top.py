a19bd72176063126bfb4a1bc7302758cba712a04
Nazar Romaniv <nazarromaniv999@gmail.com>
2017-05-26 02:23:19 +0300

    Fix Top behavior

--- modules/top.py
+++ modules/top.py
@@ -3,31 +3,39 @@ from ctypes import py_object
 
 class Top:
 
-    def __init__(self):
+    def __init__(self, param):
         CTop = py_object * 10
+        self.__param = param
         self.__elements = CTop()
         self.min = 0
         self.count = 0
+        self.__clear(None)
 
-    def add(self, new, param):
+    def __clear(self, value):
+        for i in xrange(10):
+            self.__elements[i] = value
+
+    def view(self):
+        for item, index in enumerate(self):
+            print item, index
+
+    def add(self, new):
         if self.count == 10:
-            value = eval("new.%s" % param)
-            if value < min:
+            if new < min:
                 return None
             else:
-                self.__insert(new, value)
+                self.__insert(new)
         else:
-            self.__insert(new, value)
+            self.__insert(new)
 
-    def __insert(self, new, value):
-        for index in xrange(9, -1, -1):
-            if self[index].x < value:
-                self.shift(index)
-                new.x = value
+    def __insert(self, new):
+        for index in xrange(0, 10):
+            if self[index] < new:
+                self.__shift(index)
                 self[index] = new
                 break
         if index == 9:
-            self.min = new.x
+            self.min = new
 
     def __getitem__(self, index):
         if index < 0 or index >= 10:
@@ -45,10 +53,27 @@ class Top:
         if index < 0 or index >= 10:
             raise IndexError("Top only contains top 10 items")
         else:
-            self.__pop_last
             for i in xrange(8, index, -1):
                 self[i + 1] = self[i]
 
     def __iter__(self):
+        return _TopIterator(self)
+
+
+class _TopIterator:
+
+    def __init__(self, top):
+        self.current = 0
+        self.top = top
+
+    def __iter__(self):
         return self
+
+    def next(self):
+        if self.current == 10:
+            raise StopIteration
+        else:
+            obj = self.top[self.current]
+            self.current += 1
+            return obj
 
